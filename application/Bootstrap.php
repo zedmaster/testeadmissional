44c68150ff2c520aa7ff2911161b804902a74604
zedmaster <zedmaster@gmail.com>
2012-02-26 15:13:56 -0300

    * Close  #17 - Arrumar Layout * Alterado o include do JQuery * Adicionado texto e links no index * Adicionado logo no footer

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,33 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+public function _initView() {
+        $this->bootstrap("layout");
+        $layout = $this->getResource("layout");
+        $view = $layout->getView();
+        $view->addHelperPath("ZendX/JQuery/View/Helper", "ZendX_JQuery_View_Helper");
+        $view->jQuery()->addStylesheet("/css/cupertino/jquery-ui-1.8.18.custom.css")
+            ->addStylesheet("/css/jquery.dataTables.css")
+            ->addStylesheet("/css/jquery.dataTables_themeroller.css")
+            ->setLocalPath('js/jquery-1.7.1.min.js')
+            ->setUiLocalPath("js/jquery-ui-1.8.18.custom.min.js")
+            ->addJavascriptFile('/js/jquery.dataTables.min.js')
+            ->addJavascript("$(document).ready(function(){ 
+                    $('#table1').dataTable({
+                            'bJQueryUI': true,
+                            'bPaginate': false,     
+                            'bFilter': false, 
+                            'sDom': '<\"toolbar\">frtip', 
+                            'bInfo': false,  
+                            });
+                    });");
+
+
+            $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
+            $viewRenderer->setView($view);
+            Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+            }
+
 
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,9 +5,6 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-         $this->view->addHelperPath(
-                    'ZendX/JQuery/View/Helper',
-                    'ZendX_JQuery_View_Helper');
     }
 
     public function indexAction()
